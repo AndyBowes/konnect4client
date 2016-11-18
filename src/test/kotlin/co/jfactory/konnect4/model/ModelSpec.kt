dcfc13514d7ffb1fb8e92efaae4517ce6877b14c
Andy Bowes <andy.bowes@digital.hmrc.gov.uk>
2016-11-18 18:16:30 +0000

    Addition of Spec Unit Tests

--- src/test/kotlin/co/jfactory/konnect4/model/ModelSpec.kt
+++ src/test/kotlin/co/jfactory/konnect4/model/ModelSpec.kt
@@ -1,6 +1,9 @@
 package co.jfactory.konnect4.model
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.natpryce.hamkrest.assertion.*
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.should.shouldMatch
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
@@ -8,15 +11,17 @@ import org.jetbrains.spek.api.dsl.it
 class ModelSpec : Spek({
     describe("GameState JSON Parsing") {
         val mapper = jacksonObjectMapper()
-        val json = ""
+        val json = "{\"CurrentState\":3,\"Cells\":[[0,0,0,0,0,0],[2,0,0,0,0,0],[0,0,0,0,0,0],[1,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[2,0,0,0,0,0]],\"YellowPlayerID\":\"a05bf67c-2bbb-4243-bf18-fe60c52cf4f9\", \"RedPlayerID\":\"6351a731-e934-4397-bafa-29d09cfb9dd9\", \"ID\":\"f67c2987-c803-4c79-a065-32de5455bd09\"}"
 
         it("should create GameState objects from JSON ") {
             val gameState: GameState = mapper.readValue(json, GameState::class.java)
-            assert(gameState.id.equals("f67c2987-c803-4c79-a065-32de5455bd09"))
-            assert(gameState.currentState.equals(State.RedToPlay))
-            assert(gameState.redPlayerId.equals("6351a731-e934-4397-bafa-29d09cfb9dd9"))
-            assert(gameState.yellowPlayerId.equals("a05bf67c-2bbb-4243-bf18-fe60c52cf4f9"))
-            assert(gameState.getCellContent(0, 0).equals(CellContent.EMPTY))
+            gameState.id shouldMatch equalTo("f67c2987-c803-4c79-a065-32de5455bd09")
+            gameState.currentState shouldMatch equalTo(State.RedToPlay)
+            gameState.redPlayerId shouldMatch equalTo("6351a731-e934-4397-bafa-29d09cfb9dd9")
+            gameState.yellowPlayerId shouldMatch equalTo("a05bf67c-2bbb-4243-bf18-fe60c52cf4f9")
+            gameState.getCellContent(0, 0) shouldMatch equalTo(CellContent.EMPTY)
+            gameState.getCellContent(1, 0) shouldMatch equalTo(CellContent.YELLOW)
+            gameState.getCellContent(3, 0) shouldMatch equalTo(CellContent.RED)
         }
     }
 
@@ -24,82 +29,93 @@ class ModelSpec : Spek({
         val redUser = User("RedUserID", "Team Red", "password123")
         val yellowUser = User("YellowUserID", "Team Yellow", "password999")
         val gameState = createGameState(State.GameNotStarted, redUser.playerId, yellowUser.playerId)
-        assert(!gameState.haveDrawn())
-        assert(!gameState.haveLost(redUser))
-        assert(!gameState.haveLost(yellowUser))
-        assert(!gameState.haveWon(redUser))
-        assert(!gameState.haveWon(yellowUser))
-        assert(!gameState.isMyTurn(redUser))
-        assert(!gameState.isMyTurn(yellowUser))
+        it("Should identify that Game has started") {
+            assertThat("haveDrawn", gameState.haveDrawn(),equalTo(false))
+            assertThat("Red lost", gameState.haveLost(redUser),equalTo(false))
+            assertThat("Yellow lost", gameState.haveLost(yellowUser),equalTo(false))
+            assertThat("Red won", gameState.haveWon(redUser),equalTo(false))
+            assertThat("Yellow won", gameState.haveWon(yellowUser),equalTo(false))
+            assertThat("Red's turn", gameState.isMyTurn(redUser),equalTo(false))
+            assertThat("Yellow's turn", gameState.isMyTurn(yellowUser),equalTo(false))
+        }
     }
 
     describe("Game Drawn - Status Checking") {
         val redUser = User("RedUserID", "Team Red", "password123")
         val yellowUser = User("YellowUserID", "Team Yellow", "password999")
         val gameState = createGameState(State.Draw, redUser.playerId, yellowUser.playerId)
-        assert(gameState.haveDrawn())
-        assert(!gameState.haveLost(redUser))
-        assert(!gameState.haveLost(yellowUser))
-        assert(!gameState.haveWon(redUser))
-        assert(!gameState.haveWon(yellowUser))
-        assert(!gameState.isMyTurn(redUser))
-        assert(!gameState.isMyTurn(yellowUser))
+        it("Should identify that Game has drawn") {
+            assertThat("haveDrawn", gameState.haveDrawn(),equalTo(true))
+            assertThat("Red lost", gameState.haveLost(redUser),equalTo(false))
+            assertThat("Yellow lost", gameState.haveLost(yellowUser),equalTo(false))
+            assertThat("Red won", gameState.haveWon(redUser),equalTo(false))
+            assertThat("Yellow won", gameState.haveWon(yellowUser),equalTo(false))
+            assertThat("Red's turn", gameState.isMyTurn(redUser),equalTo(false))
+            assertThat("Yellow's turn", gameState.isMyTurn(yellowUser),equalTo(false))
+        }
     }
 
     describe("Red To Play - Status Checking") {
         val redUser = User("RedUserID", "Team Red", "password123")
         val yellowUser = User("YellowUserID", "Team Yellow", "password999")
         val gameState = createGameState(State.RedToPlay, redUser.playerId, yellowUser.playerId)
-        assert(!gameState.haveDrawn())
-        assert(!gameState.haveLost(redUser))
-        assert(!gameState.haveLost(yellowUser))
-        assert(!gameState.haveWon(redUser))
-        assert(!gameState.haveWon(yellowUser))
-        assert(gameState.isMyTurn(redUser))
-        assert(!gameState.isMyTurn(yellowUser))
+        it("Should identify that Red is next to Play") {
+            assertThat("haveDrawn", gameState.haveDrawn(),equalTo(false))
+            assertThat("Red lost", gameState.haveLost(redUser),equalTo(false))
+            assertThat("Yellow lost", gameState.haveLost(yellowUser),equalTo(false))
+            assertThat("Red won", gameState.haveWon(redUser),equalTo(false))
+            assertThat("Yellow won", gameState.haveWon(yellowUser),equalTo(false))
+            assertThat("Red's turn", gameState.isMyTurn(redUser),equalTo(true))
+            assertThat("Yellow's turn", gameState.isMyTurn(yellowUser),equalTo(false))
+        }
     }
 
     describe("Yellow To Play - Status Checking") {
         val redUser = User("RedUserID", "Team Red", "password123")
         val yellowUser = User("YellowUserID", "Team Yellow", "password999")
         val gameState = createGameState(State.YellowToPlay, redUser.playerId, yellowUser.playerId)
-        assert(!gameState.haveDrawn())
-        assert(!gameState.haveLost(redUser))
-        assert(!gameState.haveLost(yellowUser))
-        assert(!gameState.haveWon(redUser))
-        assert(!gameState.haveWon(yellowUser))
-        assert(!gameState.isMyTurn(redUser))
-        assert(gameState.isMyTurn(yellowUser))
+        it("Should identify that Yellow is next to Play") {
+            assertThat("haveDrawn", gameState.haveDrawn(),equalTo(false))
+            assertThat("Red lost", gameState.haveLost(redUser),equalTo(false))
+            assertThat("Yellow lost", gameState.haveLost(yellowUser),equalTo(false))
+            assertThat("Red won", gameState.haveWon(redUser),equalTo(false))
+            assertThat("Yellow won", gameState.haveWon(yellowUser),equalTo(false))
+            assertThat("Red's turn", gameState.isMyTurn(redUser),equalTo(false))
+            assertThat("Yellow's turn", gameState.isMyTurn(yellowUser),equalTo(true))
+        }
     }
     describe("Red Win - Status Checking") {
         val redUser = User("RedUserID", "Team Red", "password123")
         val yellowUser = User("YellowUserID", "Team Yellow", "password999")
         val gameState = createGameState(State.RedWin, redUser.playerId, yellowUser.playerId)
-        assert(!gameState.haveDrawn())
-        assert(!gameState.haveLost(redUser))
-        assert(gameState.haveLost(yellowUser))
-        assert(gameState.haveWon(redUser))
-        assert(!gameState.haveWon(yellowUser))
-        assert(!gameState.isMyTurn(redUser))
-        assert(!gameState.isMyTurn(yellowUser))
+        it("Should identify that Red has Won") {
+            assertThat("haveDrawn", gameState.haveDrawn(),equalTo(false))
+            assertThat("Red lost", gameState.haveLost(redUser),equalTo(false))
+            assertThat("Yellow lost", gameState.haveLost(yellowUser),equalTo(true))
+            assertThat("Red won", gameState.haveWon(redUser),equalTo(true))
+            assertThat("Yellow won", gameState.haveWon(yellowUser),equalTo(false))
+            assertThat("Red's turn", gameState.isMyTurn(redUser),equalTo(false))
+            assertThat("Yellow's turn", gameState.isMyTurn(yellowUser),equalTo(false))
+        }
     }
 
     describe("Yellow Win - Status Checking") {
         val redUser = User("RedUserID", "Team Red", "password123")
         val yellowUser = User("YellowUserID", "Team Yellow", "password999")
         val gameState = createGameState(State.YellowWin, redUser.playerId, yellowUser.playerId)
-        assert(!gameState.haveDrawn())
-        assert(gameState.haveLost(redUser))
-        assert(!gameState.haveLost(yellowUser))
-        assert(!gameState.haveWon(redUser))
-        assert(gameState.haveWon(yellowUser))
-        assert(!gameState.isMyTurn(redUser))
-        assert(!gameState.isMyTurn(yellowUser))
+        it("Should identify that Yellow has won") {
+            assertThat("haveDrawn", gameState.haveDrawn(),equalTo(false))
+            assertThat("Red lost", gameState.haveLost(redUser),equalTo(true))
+            assertThat("Yellow lost", gameState.haveLost(yellowUser),equalTo(false))
+            assertThat("Red won", gameState.haveWon(redUser),equalTo(false))
+            assertThat("Yellow won", gameState.haveWon(yellowUser),equalTo(true))
+            assertThat("Red's turn", gameState.isMyTurn(redUser),equalTo(false))
+            assertThat("Yellow's turn", gameState.isMyTurn(yellowUser),equalTo(false))
+        }
     }
-
 })
 
-private fun createGameState(state: State, redUserId: String, yellowUserId: String): GameState {
+private fun createGameState(state: State, yellowUserId: String, redUserId: String): GameState {
     val gameState = GameState("000021212122", state,
             listOf(listOf(CellContent.YELLOW, CellContent.RED, CellContent.EMPTY),
                     listOf(CellContent.EMPTY, CellContent.EMPTY, CellContent.EMPTY),
